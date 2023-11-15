590beabaf7171717bc0aa529cdc592833ff59821
areeba <SP23-BCS-021@CUILAHORE.EDU.PK>
2023-11-15 15:20:29 +0500

    main

--- src/TaxiService/Customer.java
+++ src/TaxiService/Customer.java
@@ -8,7 +8,6 @@ public class Customer <T>{
 
     T Fair;
 
-
     public void Customer_Location(T latitude_y, T longitude_x) {
       this.latitude_y = latitude_y;
       this.longitude_x = longitude_x;
